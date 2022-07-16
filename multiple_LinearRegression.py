c80755e68cc66a78296a2176f677537c503d5318
Sayantan1718 <sayantan1718@gmail.com>
2022-07-16 23:18:38 +0530

    Profit Calculation of Startups
    Calculating the profit of startups based on various factors. Trainied and tested on dataset using Multiple Linear Regression

---
+++ multiple_LinearRegression.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  9 13:51:19 2022
+
+@author: sayan
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+#Encoding the categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])] , remainder="passthrough")
+X = np.array(ct.fit_transform(X))
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+#Training the Multiple Linear Regression on the dataset
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Prediction
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision = 2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
